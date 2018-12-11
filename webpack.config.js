c7b45db6040b15cb0c8b73b54615db7e40b26515
Trevor Burkholder <tburk83@potonmail.com>
2018-12-11 16:04:16 -0500

    MOD: upgraded to webpack 4 MOD: upgraded to React and React-DOM to 16.6 MOD: upgraded Redux to 5.1.0 ADD: @babel/polyfill MOD: added babel polyfill to webpack entrypoint for IE11 support

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,50 @@
-var path = require("path");
-var webpack = require("webpack");
-var CleanWebpackPlugin = require('clean-webpack-plugin');
+const path = require('path');
+const webpack = require('webpack');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
-module.exports = {
-    entry: {
-      'react_runtime': [path.join(__dirname, "src", "react_runtime.js")]
-    },
-    output: {
-      path: path.join(__dirname, "dist"),
-      filename: "[name].dll.js",
-      library: "[name]"
-    },
-    plugins: [
-      new webpack.DllPlugin({
-        path: 'dist/[name]-manifest.json',
-        name: "[name]"
-      }),
-      new webpack.DefinePlugin({'process.env': {NODE_ENV: JSON.stringify('production')}}),
-      new webpack.optimize.UglifyJsPlugin(),
-      new CleanWebpackPlugin(['dist'], {root: __dirname, verbose: true, dry: false}),
-    ],
+const config = {
+  entry: {
+    react_runtime: [
+      "@babel/polyfill",
+      path.join(__dirname, 'src', 'react_runtime.js')
+    ]
+  },
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: '[name].dll.js',
+    library: '[name]'
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          compress: false,
+          ecma: 6,
+          mangle: true
+        },
+        sourceMap: true
+      })
+    ]
+  },
+  plugins: [
+    new webpack.DllPlugin({
+      path: 'dist/[name]-manifest.json',
+      name: '[name]'
+    }),
+    new webpack.DefinePlugin({
+      'process.env': { NODE_ENV: JSON.stringify('development') }
+    }),
+    new CleanWebpackPlugin(['dist'], {
+      root: __dirname,
+      verbose: true,
+      dry: false
+    })
+  ],
+  devtool: 'source-map',
+  mode: 'development',
 };
+
+module.exports = config;
